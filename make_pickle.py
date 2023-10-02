743dc3a900f6db5bc39e33b5bb898de90ad54e5a
thanhleviet <thanh.le-viet@quadram.ac.uk>
2023-10-02 10:33:03 +0100

    add making pickle db

--- make_pickle.py
+++ make_pickle.py
@@ -7,11 +7,16 @@ if __name__ == '__main__':
     print("Hello World!")
     # Connect to SQLite database and query table
     conn = sqlite3.connect('genomes.db')
-    df = pd.read_sql_query('SELECT accession, genome_size FROM refseq', conn)
+    df = pd.read_sql_query('SELECT accession, description, genome_size FROM refseq', conn)
     print(df)
     # Create dictionary with accession as key, genome_size as value
-    genome_dict = df.set_index('accession')['genome_size'].to_dict() 
+    genome_dict = {}
+    for _, row in df.iterrows():
+        genome_dict[row['accession']] = {
+            'description': row['description'],
+            'genome_size': row['genome_size']
+        }
     print(genome_dict)
     # Save dictionary to a pickle file
-    with open('refseq.pkl', 'wb') as f:
+    with open('refseq_v2.pkl', 'wb') as f:
         pickle.dump(genome_dict, f)
